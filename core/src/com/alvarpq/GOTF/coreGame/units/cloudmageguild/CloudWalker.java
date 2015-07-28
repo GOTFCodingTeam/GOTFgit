0d109fc74738da0496da55b7b46dd89e05288418
LinasJusevicius <linasjusevicius@gmail.com>
2015-07-28 17:17:55 +0300

    Added Cloud Walker

---
+++ core/src/com/alvarpq/GOTF/coreGame/units/cloudmageguild/CloudWalker.java
@@ -0,0 +1,9 @@
+package com.alvarpq.GOTF.coreGame.units.cloudmageguild;
+import com.alvarpq.GOTF.coreGame.units.Unit;
+public class CloudWalker extends Unit
+{
+	public CloudWalker(int row, int column)
+	{
+		super("CloudWalker", null, 3, 2, 3, 1, true, new String[]{"Mage"}, row, column);
+	}
+}
